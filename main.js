b9be8982c6103fb58e8575ba5cb9b9a980439b1c
magdielmarques <contato.magdiel@gmail.com>
2021-11-01 14:04:58 -0400

    Event Handling

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const app = new Vue({
         product: 'Socks',
         image: './assets/black-socks.jpg',
         inStock: true,
-        inventory: 0
+        inventory:10,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'white',
+                variantImage: './assets/white-socks.jpg',
+            },
+            {
+                variantId: 2235,
+                variantColor: 'black',
+                variantImage: './assets/black-socks.jpg',
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        // anonymous functions
+        // addToCart () {
+        //     this.cart += 1
+        // }
+        addToCart: function () {
+            this.cart += 1
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        }
     }
 })
